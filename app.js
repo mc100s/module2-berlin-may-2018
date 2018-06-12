24f0204158b0973313e37c1ef4f630a23f9345eb
Maxence Bouret <maxence.bouret@gmail.com>
2018-06-12 16:36:32 +0200

    Counter added

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+let counter = 0;
+console.log("A");
+
 // We create our own server named app
 // Express server handling requests and responses
 const app = express();
@@ -7,22 +10,37 @@ const app = express();
 // Make everything inside of public/ available
 app.use(express.static('public'));
 
+console.log("B");
+
+
 // our first Route
 app.get('/', (request, response) => {
-  console.log(request);
+  console.log("R1");
   response.sendFile(__dirname + '/views/home-page.html');
 });
 
-app.get('/animals/mac', (request, response) => {
-  console.log(request);
+app.get('/animals/:name', (request, response) => {
+  counter++;
+  console.log("R2");
+  console.log(request.params);
+  let name = request.params.name;
   response.send(`
-    <h1>Hi I'm Mac!</h1>
-    <img src="/images/mac.jpg">
+    <html>
+    <head>
+      <title></title>
+    </head>
+    <h1>Hi I'm ${name}!</h1>
+    <p>This page has been displayed ${counter} times</p>
   `);
 });
+
+console.log("C");
+
 
 
 // Server Started
 app.listen(3000, () => {
   console.log('My first app listening on port 3000!')
 });
+
+console.log("D");
