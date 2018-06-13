27a994cf2b7cfac26138b370724b8a8f0e84f757
Maxence Bouret <maxence.bouret@gmail.com>
2018-06-13 16:35:19 +0200

    Version with Layout and Partials

--- app.js
+++ app.js
@@ -4,23 +4,46 @@ const path = require('path');
 
 const app = express();
 
+const animals = [
+  { name: "Rex", type: "dog", pictureUrl: "https://images.pexels.com/photos/58997/pexels-photo-58997.jpeg?auto=compress&cs=tinysrgb&h=350" },
+  { name: "Mac", type: "dog", pictureUrl: "https://i.imgur.com/LkI32wu.jpg"},
+  { name: "Nano", type: "dog", pictureUrl: "https://images.pexels.com/photos/101635/pexels-photo-101635.jpeg?auto=compress&cs=tinysrgb&h=350"}
+]
+
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 
 app.get('/', (req, res) => {
   let data = {
-    person: {
-      firstName: "Silvio"
-    },
-    address: {
-      city: "Berlin",
-      country: "Germany",
-    }
+    pets: animals,
+    isHomeActive: "active"
   }
-  data.isCityBerlin = data.city === "Berlin";
   res.render('home', data);
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/:a/:b/:c', (req, res) => {
+  console.log('DEBUG req.params', req.params);
+  res.render('about');
+});
+
+app.get('/pets/:index', (req, res) => {
+  let index = Number(req.params.index);
+
+  if (Number.isNaN(index) || index >= animals.length) {
+    res.redirect('/');
+  }
+  else {
+    let data = animals[index];
+    res.render('pets/profile', data);
+  }
 });
 
 app.listen(3000, () => {
